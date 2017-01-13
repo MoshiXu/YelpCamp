fb1f55a59f2ed7267a87a39a3684c3273b5005f3
nmabachi <nmabachi@gmail.com>
2017-01-12 19:19:15 -0500

    Implemented new campground form and POST route.  Installed body-parser to handle form submission.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,16 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', (req, res) => {
   res.render("campgrounds", {campgrounds});
+});
+
+app.get('/newCampground', (req, res) => {
+  res.render("newCampgroundForm");
+});
+
+app.post('/newCampground', (req, res) => {
+  var name = req.body.campName;
+  campgrounds.push(name);
+  res.redirect('/campgrounds');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
