b34953ab81be1c41b4ad5e776c6ac377ae2cc6b0
nmabachi <nmabachi@gmail.com>
2017-01-12 19:04:02 -0500

    Implemented ejs and partials.  Basic landing page and CG index added.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+var campgrounds = ["Yosemite", "Denali", "Bryce Canyon", "South Rim"];
 
 app.get('/', (req, res) => {
-  res.send("HEYO");
+  res.render("index");
+});
+
+app.get('/campgrounds', (req, res) => {
+  res.render("campgrounds", {campgrounds});
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
