7f4a2dc3af5029171ce0068199cebae43bcad840
nmabachi <nmabachi@gmail.com>
2017-01-13 20:32:04 -0500

    Implemented Mongoose for DB handling.  Changed views to match naming conventions. TODO: implement models, schema currently stored in main app.

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+mongoose.connect('mongodb://localhost/yelpcamp');
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-const PORT = process.env.PORT || 3000;
 
-// Connection URL
-var url = `mongodb://localhost:27017/yelpcamp`;
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  db.close();
+//REFACTOR INTO MODELS  ///////////////////////////////////////
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
 });
 
-var campgrounds = [
-  {
-    name: "Yosemite",
-    image: "https://static.pexels.com/photos/104864/pexels-photo-104864.jpeg"
-  },
-  {
-    name: "Denali",
-    image: "https://static.pexels.com/photos/48638/pexels-photo-48638.jpeg"
-  },
-  {
-    name: "Bryce Canyon",
-    image: "https://static.pexels.com/photos/25543/pexels-photo-25543.jpg"
-  },
-  {
-    name: "South Rim",
-    image: "https://static.pexels.com/photos/27865/pexels-photo-27865.jpg"
-  }];
+var Campground = mongoose.model("Campground", campgroundSchema);
+////////////////////////////////////////////////////////////////
+
+
 
 app.get('/', (req, res) => {
-  res.render("index");
+  res.render("landing");
 });
 
 app.get('/campgrounds', (req, res) => {
-  res.render("campgrounds", {campgrounds});
+  Campground.find({}, (err, campgrounds) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("index", {campgrounds});
+    }
+  });
 });
 
 app.get('/campgrounds/new', (req, res) => {
-  res.render("newCampgroundForm");
+  res.render("new");
 });
 
 app.post('/campgrounds', (req, res) => {
-  var {campName, campImage} = req.body;
-  var newCamp = {
+  var {campName, campImage, campDescription} = req.body;
+  Campground.create({
     name: campName,
-    image: campImage
-  };
+    image: campImage,
+    description: campDescription
+  }, (err, campground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
+});
 
-  campgrounds.push(newCamp);
-  res.redirect('/campgrounds');
+app.get('/campgrounds/:id', (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("show", {campground});
+    }
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
